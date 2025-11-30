b4375f660ee65c9bc74441df783da38c0f60fba8
colmehurze-tech <colmehurze@gmail.com>
2025-11-30 21:39:14 +0530

    Change compression type from xz to gzip

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -13,10 +13,10 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 # Set the compression type
-_squashfscomp=('gzip')
+#_squashfscomp=('gzip')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'gzip' '-b' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
